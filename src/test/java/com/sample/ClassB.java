8bcc5c358d3835192086808213f15826e12a005b
RagaviAravindhan <ragaviaravindhan@gmail.com>
2025-04-23 08:36:11 +0530

    Added code from Tester 1

---
+++ src/test/java/com/sample/ClassB.java
@@ -0,0 +1,19 @@
+package com.sample;
+
+public class ClassB {
+
+	private void java() {
+		System.out.println("JAVA");
+
+	}
+
+	private void python() {
+		System.out.println("PYTHON");
+
+	}
+
+	private void ruby() {
+		System.out.println("RUBY");
+	}
+
+}
